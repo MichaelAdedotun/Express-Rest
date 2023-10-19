cd567dc3c38108e6e71fb77771675b948b26e262
MichealAdeotun <adedotun2096@gmail.com>
2023-10-19 21:25:44 +0100

    added files to index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,15 @@
 import {createRouter, createWebHashHistory} from "vue-router";
 import Home from "../view/Home.vue";
+import Contact from "../view/Contact.vue";
+import Facilities from "../view/Facilities.vue";
+import Room from "../view/Room.vue";
 
 
 const routes = [
     { path: '/', component: Home },
-
+    { path: '/contact', component: Contact },
+    { path: '/facilities', component: Facilities },
+    { path: '/rooms', component: Room },
 ]
 
 
