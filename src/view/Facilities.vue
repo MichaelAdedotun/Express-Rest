7d8debb624361738430657b0971de7044ecbdf2e
MichealAdeotun <adedotun2096@gmail.com>
2023-10-19 21:31:31 +0100

    added facilities page

--- src/view/Facilities.vue
+++ src/view/Facilities.vue
@@ -3,7 +3,88 @@
 </script>
 
 <template>
-  $END$
+  <section class="bg-[url('/src/assets/facility.jpg')] bg-cover bg-center bg-no-repeat text-white">
+    <div class="px-14 md:px-20 py-56 animate-pulse">
+      <h2 class="text-4xl md:text-5xl font-['Playfair'] font-semibold">
+        The Best Facilities <br>
+        You Can Get Around
+      </h2>
+      <p class="font-light">
+        Our facilities are made and tuned to fit your lifestyle.
+      </p>
+    </div>
+  </section>
+
+  <section class="px-14 py-20">
+    <div class="mb-10 text-nav">
+      <h2 class="text-3xl">
+        Facilities
+      </h2>
+      <p class="text-sm my-3">
+        Our resort is equipped with ultra-modern facilities and equipment <br>
+        that would make your stay worth-while.
+      </p>
+    </div>
+
+    <div class="grid gap-5 md:grid-cols-3 text-nav">
+      <div class="">
+        <img src="/src/assets/Facilities/basketball.jpg" class="w-full md:h-64" alt="">
+        <p class="text-center">
+          Basketball Court
+        </p>
+      </div>
+      <div class="">
+        <img src="/src/assets/Facilities/wifi.jpg" class="w-full md:h-64" alt="">
+        <p class="text-center">
+          Free Internet
+        </p>
+      </div>
+      <div class="">
+        <img src="/src/assets/Facilities/spa.jpg" class="w-full md:h-64" alt="">
+        <p class="text-center">
+          Spa
+        </p>
+      </div>
+      <div class="">
+        <img src="/src/assets/Facilities/conference-room.jpg" class="w-full md:h-64" alt="">
+        <p class="text-center">
+          Conference Room
+        </p>
+      </div>
+      <div class="">
+        <img src="/src/assets/Facilities/bar.jpg" class="w-full md:h-64" alt="">
+        <p class="text-center">
+          Bar
+        </p>
+      </div>
+      <div class="">
+        <img src="/src/assets/Facilities/parking-lot.jpg" class="w-full md:h-64" alt="">
+        <p class="text-center">
+          Parking Lot
+        </p>
+      </div>
+      <div class="">
+        <img src="/src/assets/Facilities/swimming-pool.jpg" class="w-full md:h-64" alt="">
+        <p class="text-center">
+          Swimming Pool
+        </p>
+      </div>
+      <div class="">
+        <img src="/src/assets/Facilities/gym.jpg" class="w-full md:h-64" alt="">
+        <p class="text-center">
+          Gym
+        </p>
+      </div>
+      <div class="">
+        <img src="/src/assets/Facilities/golf.jpg" class="w-full md:h-64" alt="">
+        <p class="text-center">
+          Golf Course
+        </p>
+      </div>
+    </div>
+  </section>
+
+
 </template>
 
 <style scoped>
