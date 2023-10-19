16dcd213612240faa01ea58ec9487e151ba4339d
MichealAdeotun <adedotun2096@gmail.com>
2023-10-19 21:32:38 +0100

    added contact page

--- src/view/Contact.vue
+++ src/view/Contact.vue
@@ -3,7 +3,70 @@
 </script>
 
 <template>
-  $END$
+    <section class="bg-[url('/src/assets/contact.jpg')] bg-cover bg-center bg-no-repeat text-white">
+      <div class="px-14 md:px-20 py-56 animate-pulse">
+        <h2 class="text-4xl md:text-5xl font-['Playfair'] font-semibold">
+          We Are Always Here
+        </h2>
+        <p class="font-light">
+          You can reach us regardless of where you are.
+        </p>
+      </div>
+    </section>
+    <section class="p-14 md:p-20">
+      <h2 class="text-3xl font-semibold mb-5">
+        Contact
+      </h2>
+      <section class="flex flex-col gap-5 md:flex-row text-nav">
+        <div class="md:w-1/2">
+          <p class="mb-5 text-sm">
+            Take advantage of the availability of pur support team always
+            working 24/7 ensuring that your questions are answered and
+            you never have to leave with doubts.
+          </p>
+          <ul>
+            <li class="flex gap-3 items-center text-sm mb-3">
+              <svg xmlns="http://www.w3.org/2000/svg" width="21" height="20" viewBox="0 0 21 20" fill="none">
+                <path d="M10.4998 0.473389C4.71002 0.473389 0 3.69435 0 7.65218C0 13.58 9.80065 19.2719 10.218 19.5113C10.296 19.5567 10.3949 19.5818 10.4974 19.5822C10.5998 19.5827 10.6992 19.5584 10.778 19.5137C11.1954 19.2785 20.9995 13.6845 20.9995 7.65218C20.9995 3.69435 16.2895 0.473389 10.4998 0.473389ZM10.5015 18.8879C8.89067 17.9056 0.87498 12.7619 0.87498 7.65218C0.87498 4.02278 5.193 1.07053 10.4998 1.07053C15.8065 1.07053 20.1245 4.02278 20.1245 7.65218C20.1245 12.8485 12.1167 17.9188 10.5015 18.8879Z" fill="black"/>
+                <path d="M10.4999 4.05621C7.60457 4.05621 5.25 5.66311 5.25 7.63904C5.25 9.61497 7.60457 11.2219 10.4999 11.2219C13.3952 11.2219 15.7498 9.61497 15.7498 7.63904C15.7498 5.66311 13.3952 4.05621 10.4999 4.05621ZM10.4999 10.6247C8.08756 10.6247 6.12498 9.28535 6.12498 7.63904C6.12498 5.99273 8.08756 4.65335 10.4999 4.65335C12.9122 4.65335 14.8748 5.99273 14.8748 7.63904C14.8748 9.28535 12.9122 10.6247 10.4999 10.6247Z" fill="black"/>
+              </svg>
+              Plot 134 Adeoumbi Williams Street, Victoria Island, Lagos Nigeria
+            </li>
+            <li class="flex gap-3 items-center text-sm mb-3">
+              <svg xmlns="http://www.w3.org/2000/svg" width="21" height="15" viewBox="0 0 21 15" fill="none">
+                <path d="M18.5778 14.7526H18.4405C2.57521 14.1297 0.322574 4.99055 0.0076903 2.20139C-0.0176819 1.98453 0.0198728 1.76638 0.118202 1.55945C0.216532 1.35251 0.373704 1.16086 0.580713 0.995467C0.787721 0.830073 1.0405 0.694191 1.32456 0.595607C1.60862 0.497022 1.91838 0.437673 2.2361 0.42096H6.68485C7.00826 0.420746 7.32431 0.486836 7.59206 0.610668C7.85982 0.7345 8.06694 0.910369 8.1866 1.11549L9.41384 3.17705C9.53201 3.37744 9.56133 3.59712 9.49817 3.80879C9.43501 4.02046 9.28216 4.21481 9.05859 4.36767L7.33884 5.55279C7.60747 6.595 8.33854 7.55573 9.43314 8.305C10.5277 9.05426 11.933 9.5559 13.4589 9.74204L15.2109 8.55692C15.4382 8.40598 15.7256 8.30405 16.0371 8.26386C16.3487 8.22367 16.6707 8.247 16.963 8.33093L20.0069 9.16326C20.3028 9.24754 20.5553 9.39005 20.7321 9.57264C20.909 9.75523 21.0022 9.96963 21 10.1885V13.099C21 13.5375 20.7448 13.9581 20.2905 14.2683C19.8363 14.5784 19.2202 14.7526 18.5778 14.7526ZM2.42988 1.52339C2.21574 1.52339 2.01038 1.58147 1.85896 1.68484C1.70755 1.78822 1.62248 1.92842 1.62248 2.07461V2.11871C1.99388 5.38191 4.3757 13.099 18.5293 13.6502C18.6354 13.6546 18.7417 13.6448 18.8422 13.6211C18.9427 13.5975 19.0353 13.5606 19.1148 13.5124C19.1943 13.4643 19.2592 13.4059 19.3055 13.3406C19.3519 13.2753 19.379 13.2045 19.3852 13.132V10.1885L16.3413 9.35619L14.0241 10.9272L13.6365 10.8941C6.61218 10.2933 5.65946 5.49767 5.65946 5.44806L5.61101 5.18348L7.90401 3.60148L6.69292 1.52339H2.42988Z" fill="black"/>
+              </svg>
+              +234 1 234500-10, +234 1 2347859
+            </li>
+            <li class="flex gap-3 items-center text-sm mb-3">
+              <svg xmlns="http://www.w3.org/2000/svg" width="22" height="14" viewBox="0 0 22 14" fill="none">
+                <path d="M3 1.59128H19C20.1 1.59128 21 2.20549 21 2.9562V11.1457C21 11.8964 20.1 12.5106 19 12.5106H3C1.9 12.5106 1 11.8964 1 11.1457V2.9562C1 2.20549 1.9 1.59128 3 1.59128Z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M21 2.95621L11 7.73342L1 2.95621" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+              </svg>
+              <span>
+              <a href="">resort@expressrest.com</a> ,
+              <a href="">reservation@expressrest.com</a>
+            </span>
+            </li>
+          </ul>
+        </div>
+        <div class="md:w-1/2">
+          <form action="">
+            <div class="flex flex-col md:flex-row gap-3 mb-3">
+              <input type="text" placeholder="Name" class="border-2 border-nav w-full outline-0 px-3 py-1">
+              <input type="email" placeholder="Email" class="border-2 border-nav w-full outline-0 px-3 py-1">
+            </div>
+            <input type="text" placeholder="Subject" class="border-2 border-nav w-full outline-0 px-3 py-1 mb-3">
+            <textarea class="w-full border-2 border-nav outline-0 px-2 h-32 text-gray-400">Type your message here</textarea>
+            <button class="bg-button py-1 px-4">
+              <a class="text-white rounded-sm text-sm">
+                Send
+              </a>
+            </button>
+          </form>
+        </div>
+      </section>
+    </section>
 </template>
 
 <style scoped>
